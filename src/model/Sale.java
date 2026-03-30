5c1d346ffbf507a137421c0daed3bd637d479be0
Nono <nolan.080405@outlook.be>
2026-03-30 17:00:31 +0200

    ADD : MODEL and UPDATE MODEL

--- src/model/Sale.java
+++ src/model/Sale.java
@@ -1,4 +1,21 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Sale {
+    private LocalDate date;
+    private String paymentMethod;
+    private String state;
+    private Vehicle vehicle;
+    private Customer customer;
+    private Double price;
+
+     public Sale(LocalDate date, String paymentMethod, String state, Vehicle vehicle, Customer customer, Double price) {
+        this.date = date;
+        this.paymentMethod = paymentMethod;
+        this.state = state;
+        this.vehicle = vehicle;
+        this.customer = customer;
+        this.price = price;
+    }
 }
--- src/model/Vehicle.java
+++ src/model/Vehicle.java
@@ -1,4 +1,55 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Vehicle {
+    private String VIN;
+    private Double kilometer;
+    private LocalDate arrivalDate;
+    private Double sale_price;
+    private Double purshase_price;
+    private String registrationNumber;
+    private Integer power;
+    private String gearBox;
+    private Integer gearNumber;
+    private Integer doorNumber;
+    private Integer seatNumber;
+    private String information;
+    private Integer euroStandard;
+    private Integer yearOfProduction;
+    private Boolean isVatDeductible;
+    private Garanty garanty;
+    private Color color;
+    private Energy energy;
+    private Brand brand;
+    private State state;
+    private Customer saler;
+
+     public Vehicle(String VIN, Double kilometer, LocalDate arrivalDate, Double sale_price, Double purshase_price,
+            String registrationNumber, Integer power, String gearBox, Integer gearNumber, Integer doorNumber,
+            Integer seatNumber, String information, Integer euroStandard, Integer yearOfProduction,
+            Boolean isVatDeductible, Garanty garanty, Color color, Energy energy, Brand brand, State state,
+            Customer saler) {
+        this.VIN = VIN;
+        this.kilometer = kilometer;
+        this.arrivalDate = arrivalDate;
+        this.sale_price = sale_price;
+        this.purshase_price = purshase_price;
+        this.registrationNumber = registrationNumber;
+        this.power = power;
+        this.gearBox = gearBox;
+        this.gearNumber = gearNumber;
+        this.doorNumber = doorNumber;
+        this.seatNumber = seatNumber;
+        this.information = information;
+        this.euroStandard = euroStandard;
+        this.yearOfProduction = yearOfProduction;
+        this.isVatDeductible = isVatDeductible;
+        this.garanty = garanty;
+        this.color = color;
+        this.energy = energy;
+        this.brand = brand;
+        this.state = state;
+        this.saler = saler;
+    }
 }
