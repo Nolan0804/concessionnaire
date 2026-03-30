28a0c5ef782510e3c05bf4df01c38b4aee38dd52
Mangart <simonsekri@gmail.com>
2026-03-30 23:50:24 +0200

    Correction setYearCreated
    Changement des if en else if dans setYearCreated

--- src/model/Brand.java
+++ src/model/Brand.java
@@ -23,17 +23,12 @@ public class Brand {
 
 
     public void setYearCreated(Integer yearCreated) throws InvalidInputException {
+        int currentYear = Year.now().getValue();
         if (yearCreated == null) {
             throw new InvalidInputException("L'année ne peut pas être nulle.");
-        }
-
-        if (yearCreated < 1886) { 
+        } else if (yearCreated < 1886) { 
             throw new InvalidInputException("La première voiture a été créée en 1886. Merci d'entrer une date postérieure.");
-        }
-
-        int currentYear = Year.now().getValue();
-
-        if (yearCreated > currentYear) {
+        } else if (yearCreated > currentYear) {
             throw new InvalidInputException("Impossible d'ajouter une marque qui n'existe pas encore.");
         }
         this.yearCreated = yearCreated;
