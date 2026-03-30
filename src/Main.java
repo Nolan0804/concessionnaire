15c7cbdb2cdd6e1e9aed04488e7d9fae82c681c0
Nono <nolan.080405@outlook.be>
2026-03-30 20:35:57 +0200

    ADD : MainFrame View

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+import view.MainFrame;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        JFrame frame = new JFrame("Concessionnaire SiNo");
+
+        frame.setSize(1080, 600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setContentPane(new MainFrame()); // 👈 ta page ici
+
+        frame.setVisible(true);
     }
 }
---
+++ src/view/MainFrame.java
@@ -0,0 +1,10 @@
+package view;
+import javax.swing.*;
+
+public class MainFrame extends JPanel {
+
+    public MainFrame() {
+        JLabel label = new JLabel("Hello !");
+        this.add(label);
+    }
+}
