3681af6e293388eaaf5b3fa22ac06189561e5ce6
Mangart <simonsekri@gmail.com>
2026-03-30 23:33:38 +0200

    Correction structure setYearCreated
    Correction pour rendre le code plus clean.

--- src/model/Brand.java
+++ src/model/Brand.java
@@ -23,25 +23,20 @@ public class Brand {
 
 
     public void setYearCreated(Integer yearCreated) throws InvalidInputException {
-    if (yearCreated == null) {
-        throw new InvalidInputException("L'année ne peut pas être nulle.");
-    }
+        if (yearCreated == null) {
+            throw new InvalidInputException("L'année ne peut pas être nulle.");
+        }
 
+        if (yearCreated < 1886) { 
+            throw new InvalidInputException("La première voiture a été créée en 1886. Merci d'entrer une date postérieure.");
+        }
 
-    if (yearCreated < 1886) { 
-        throw new InvalidInputException(
-            "La première voiture a été créée en 1886. Merci d'entrer une date postérieure."
-        );
-    }
+        int currentYear = Year.now().getValue();
 
-    int currentYear = Year.now().getValue();
-
-    if (yearCreated > currentYear) {
-        throw new InvalidInputException(
-            "Impossible d'ajouter une marque qui n'existe pas encore."
-        );
-    }
-    this.yearCreated = yearCreated;
+        if (yearCreated > currentYear) {
+            throw new InvalidInputException("Impossible d'ajouter une marque qui n'existe pas encore.");
+        }
+        this.yearCreated = yearCreated;
     }
 
 
