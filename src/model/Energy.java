0b22a555430c26cbd28da17d43ce7279694568cc
Nono <nolan.080405@outlook.be>
2026-04-02 18:16:36 +0200

    UPDATE : Energy : GETTER and SETTER

--- src/model/Energy.java
+++ src/model/Energy.java
@@ -1,11 +1,26 @@
 package model;
 
+import exception.InvalidInputException;
+
 public class Energy {
-    private String type;
+    private String name;
     private Boolean isEcoFriendly;
 
-    public Energy(String type, Boolean isEcoFriendly) {
-        this.type = type;
+    public Energy(String name, Boolean isEcoFriendly) {
+        setName(name);
         this.isEcoFriendly = isEcoFriendly;
+    }
+
+    public Boolean getEcoFriendly() {
+        return isEcoFriendly;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) throws InvalidInputException{
+        if (name == null || name.isEmpty()) {
+            throw new InvalidInputException("Le type d'énergie ne peux être vide !");
+        }
     }
 }
