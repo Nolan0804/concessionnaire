2560224bb1b990e7f056b1547fc8861294c3e6a4
Nono <nolan.080405@outlook.be>
2026-04-02 16:56:26 +0200

    MAIN.JAVA

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,15 @@ import view.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
-
-        JFrame frame = new JFrame("Concessionnaire SiNo");
-
-        frame.setSize(1080, 600);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        frame.setContentPane(new MainFrame()); // 👈 ta page ici
-
-        frame.setVisible(true);
+        JFrame mainFrame = new JFrame("Concessionnaire SiNo");
+        ImageIcon icon = new ImageIcon(
+            Main.class.getResource("/img/logo.png")
+        );
+        mainFrame.setIconImage(icon.getImage());
+        mainFrame.setSize(1080, 600);
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.setContentPane(new MainFrame());
+        mainFrame.setVisible(true);
     }
 }
