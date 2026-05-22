9a2611bdb4c184d5933e996f2b5a0fbf3b1ef162
Nono <nolan.080405@outlook.be>
2026-05-22 22:30:53 +0200

    AppConfig finished

--- src/config/AppConfig.java
+++ src/config/AppConfig.java
@@ -1,6 +1,20 @@
 package config;
 
 public class AppConfig {
+    public static final int GEARBOX_LOWEST = 5;
+    public static final int GEARBOX_HIGHEST = 8;
+
+    public static final int DOOR_LOWEST = 3;
+    public static final int DOOR_HIGHEST = 5;
+
+    public static final int SEAT_LOWEST = 1;
+    public static final int SEAT_HIGHEST = 9;
+
+    public static final int EURO_STANDARD_LOWEST = 1;
+    public static final int EURO_STANDARD_HIGHEST = 7;
+
+    public static final int YEAR_PRODUCTION_LOWEST = 1886;
+
     public static final String[] COLOR_TYPES = {
             "Glossy",
             "Matte",
