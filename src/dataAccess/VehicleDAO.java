37ca6fe248bae4e11f385547a4d6d24db878276d
simon sekri <simonsekri@gmail.com>
2026-05-24 17:49:54 +0200

    ADD : VehicleDAO : Ajout de la déclaration searchSales

--- src/dataAccess/VehicleDAO.java
+++ src/dataAccess/VehicleDAO.java
@@ -5,6 +5,7 @@ import exception.InvalidInputException;
 import model.Vehicle;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface VehicleDAO {
@@ -16,4 +17,5 @@ public interface VehicleDAO {
     void updateVehicle(Vehicle vehicle) throws DataAccessException;
     Vehicle getVehicleByVIN(String vin) throws DataAccessException, InvalidInputException;
     List<Object[]> searchTrials(String energy, double maxKilometer, boolean isPotentialBuyer) throws DataAccessException;
+    List<Object[]> searchSales(LocalDate dateStart, LocalDate dateEnd, double maxPrice, String paymentMethod) throws DataAccessException;
 }
